de8fa1c754eaa8d62f848d6f4c49f9a89304985d
wohu8292 <108155439+wohu8292@users.noreply.github.com>
2022-08-25 12:28:02 -0600

    Counter

--- Counter.js
+++ Counter.js
@@ -1,6 +1,7 @@
 let num=document.getElementById("pop")
 console.log(num)
 let count=0
+let saving=document.getElementById("saved")
 
 function counter_plus(){
     count=count+1
@@ -10,4 +11,11 @@ function counter_plus(){
 function counter_minus(){
     count=count-1
     num.innerText=count
+}
+
+function record(){
+    let counterStr = count + " : "
+    saving.textContent += counterStr
+    num.textContent=0
+    count=0
 }
