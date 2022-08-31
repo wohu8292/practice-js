ba653781eb6c8dc3c02084fd2bed7f875f0ee146
wohu8292 <108155439+wohu8292@users.noreply.github.com>
2022-08-31 12:38:39 -0600

    chrome_extension (pending)

--- Chrome_Extension.js
+++ Chrome_Extension.js
@@ -1,8 +1,30 @@
 const btn_El=document.getElementById("btn_el")
-let myLeads=[]
 const input_El=document.getElementById("input_el")
+const ul_El=document.getElementById("ul_el")
+let myLeads=[]
 
 btn_El.addEventListener("click",function(){
-    myLeads.push("www.awsomelead.com")
-    console.log(myLeads)
+    myLeads.push(input_El.value)
+    input_El.value=""
+    renderLeads()
 })
+
+// for (let i=0; i<myLeads.length; i++){
+//     ul_El.innerHTML+="<li>"+myLeads[i]+"</li>"
+// }
+
+
+
+function renderLeads(){
+    let listItems=""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ul_El.innerHTML=listItems
+}
