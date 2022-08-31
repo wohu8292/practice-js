b6b4663c852afdac386026a549ad864044530178
wohu8292 <108155439+wohu8292@users.noreply.github.com>
2022-08-31 16:28:12 -0600

    Chrome_extension

--- Chrome_Extension.js
+++ Chrome_Extension.js
@@ -1,12 +1,26 @@
 const btn_El=document.getElementById("btn_el")
 const input_El=document.getElementById("input_el")
 const ul_El=document.getElementById("ul_el")
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+const delete_El=document.getElementById("delete_el")
 let myLeads=[]
+let oldLeads=[] 
+
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    render(myLeads)
+}
 
 btn_El.addEventListener("click",function(){
     myLeads.push(input_El.value)
     input_El.value=""
-    renderLeads()
+    render(myLeads)
+})
+
+delete_El.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads)   
 })
 
 // for (let i=0; i<myLeads.length; i++){
@@ -15,13 +29,13 @@ btn_El.addEventListener("click",function(){
 
 
 
-function renderLeads(){
+function render(leads){
     let listItems=""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
